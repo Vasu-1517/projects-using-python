5d0d3065a7f77bec3b2a097fba4d0df2858f8aee
Vasundhara P <payasamvasundhara@gmail.com>
2024-05-04 21:57:29 +0530

    commit the qr code

---
+++ qrcodegen.py
@@ -0,0 +1,11 @@
+
+import qrcode as qr
+# img=qr.make("https://chat.openai.com/c/c96ec8ea-cd62-4d48-b15e-2d3640609828")
+# img.save("qrcode.png")
+
+
+img=qr.make("www.linkedin.com/in/17bgn")
+img.save("nagendralinkdin.png")
+
+
+from PIL import image
